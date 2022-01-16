07304fbd83fbd7eb7ec543a28603c9650d589802
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-01-17 00:36:22 +0530

    Pass values to edit user popup form

--- js/UserManagement.js
+++ js/UserManagement.js
@@ -1,14 +1,37 @@
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal"); // Popup form
 
-// Get the button that opens the modal
-var btn = document.getElementById("addNewButton");
+var btn = document.getElementById("addNewButton"); // Add new user button
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0]; // <span> element that closes the modal
+
+var editBtn = document.getElementById("editUser"); // Edit user button
+
+var modalSID = document.getElementById("modelSID"); // SID row in the popup form
+
+var modalNote = document.getElementById("modelNote"); // Note in the popup form
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+    modalSID.style.display = "none";
+    modalNote.style.display = "block";
+    document.getElementById("uName").value = "";
+    document.getElementById("uAddress").value = "";
+    document.getElementById("uTelNo").value = "";
+    document.getElementById("uEmail").value = "";
+    document.getElementById("uNIC").value = "";
+}
+
+function editUser(sid, name, address, pNo, email, nic) {
+    modal.style.display = "block";
+    modalSID.style.display = "block";
+    modalNote.style.display = "none";
+    document.getElementById("uSID").innerHTML = sid;
+    document.getElementById("uName").value = name;
+    document.getElementById("uAddress").value = address;
+    document.getElementById("uTelNo").value = pNo;
+    document.getElementById("uEmail").value = email;
+    document.getElementById("uNIC").value = nic;
 }
 
 // When the user clicks on <span> (x), close the modal
--- php/SalesPerson.php
+++ php/SalesPerson.php
@@ -10,7 +10,11 @@
 
     <h2>Sales Person</h2>
     <!-- Add a new Sales person -->
-    <button id="addNewButton">Add new Sales Person</button>
+    <button id="addNewButton">
+        <span><strong>&#43;</strong></span>
+        Add new Sales Person
+    </button>
+
     <!-- Popup form -->
     <div id="myModal" class="modal">
         <div class="modal-content">
@@ -20,34 +24,40 @@
                     <h2>Add new User</h2>
                 </div>
                 <div class="modal-body">
+                    <span class="special">Required *</span>
                     <table>
-                        <tr>
-                            <td>Name</td>
+                        <tr id="modelSID" style="display: none;">
+                            <td>ID</span></td>
                             <td>:</td>
-                            <td><input type="text" id="uName" name="uName"></td>
+                            <td><label for="SID" id="uSID"></label></td>
                         </tr>
                         <tr>
-                            <td>Address</td>
+                            <td>Name <span class="special">*</span></td>
                             <td>:</td>
-                            <td><input type="text" id="uAddress" name="uAddress"></td>
+                            <td><input type="text" id="uName" name="uName" required></td>
                         </tr>
                         <tr>
-                            <td>Tel No</td>
+                            <td>Address <span class="special">*</span></td>
                             <td>:</td>
-                            <td><input type="number" min="0" id="uTelNo" name="uTelNo"></td>
+                            <td><input type="text" id="uAddress" name="uAddress" required></td>
                         </tr>
                         <tr>
-                            <td>Email</td>
+                            <td>Tel No <span class="special">*</span></td>
                             <td>:</td>
-                            <td><input type="email" id="uEmail" name="uEmail"></td>
+                            <td><input type="number" min="0" id="uTelNo" name="uTelNo" required></td>
                         </tr>
                         <tr>
-                            <td>NIC</td>
+                            <td>Email <span class="special">*</span></td>
                             <td>:</td>
-                            <td><input type="text" id="uNIC" name="uNIC"></td>
+                            <td><input type="email" id="uEmail" name="uEmail" required></td>
+                        </tr>
+                        <tr>
+                            <td>NIC <span class="special">*</span></td>
+                            <td>:</td>
+                            <td><input type="text" id="uNIC" name="uNIC" required></td>
                         </tr>
                     </table>
-                    <p>Default password for the account: quickeats123</p>
+                    <p id="modelNote">Default password for the account: <span class="special">quickeats123</span></p>
                 </div>
                 <div class="modal-footer">
                     <input type="submit" value="Add User">
@@ -72,18 +82,35 @@
         </tr>
         <?php
         require 'actions/dbConfig.php';
+
+        // Select all data of sales people from staff, sales_person and sales_person_phone tables
         $sql = "SELECT * FROM `sales_person` JOIN sales_person_phone ON sales_person.SID = sales_person_phone.SID JOIN staff ON staff.SID=sales_person.SID";
         $result = mysqli_query($con, $sql);
 
         while ($row = $result->fetch_assoc()) {
         ?>
+            <!-- <script>
+                var sid = <?php echo $row["SID"]; ?>;
+                var uname = "<?php echo $row["Name"]; ?>";
+                var address = "<?php echo $row["Address"]; ?>";
+                var pNo = <?php echo "0" . $row["PhoneNumber"]; ?>;
+                var email = "<?php echo $row["Email"]; ?>";
+                var nic = "<?php echo $row["NIC"]; ?>";
+            </script> -->
             <tr>
-                <td><?php echo $row["SID"]; ?></td>
-                <td><?php echo $row["Name"]; ?></td>
-                <td><?php echo $row["Address"]; ?></td>
-                <td><?php echo "0" . $row["PhoneNumber"]; ?></td>
-                <td><?php echo $row["Email"]; ?></td>
-                <td><?php echo $row["NIC"]; ?></td>
+                <td id="showSID"><?php echo $row["SID"]; ?></td>
+                <td id="showName"><?php echo $row["Name"]; ?></td>
+                <td id="showAddress"><?php echo $row["Address"]; ?></td>
+                <td id="showPNo"><?php echo "0" . $row["PhoneNumber"]; ?></td>
+                <td id="showEmail"><?php echo $row["Email"]; ?></td>
+                <td id="showNIC"><?php echo $row["NIC"]; ?></td>
+                <td>
+                    <!-- <button id="editUser" onclick="editUser(sid, uname, address, pNo, email, nic)">Edit</button> -->
+                    <?php
+                    echo "<button onclick=" . '"editUser(' . $row["SID"] . ", '" . $row["Name"] . "', '" . $row["Address"] . "', '0" . $row["PhoneNumber"] . "', '" . $row["Email"] . "', '" . $row["NIC"] . "'" . ')"' . ">Edit</button>"
+                    ?>
+                    <button id="deleteUser">Delete</button>
+                </td>
             </tr>
         <?php
         }
